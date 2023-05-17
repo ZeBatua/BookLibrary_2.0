e3d5c3f448a59846ea62ab289162d5bcad6f488a
thalassa_home <blinov.gosha@mail.ru>
2023-05-17 17:59:45 +0300

    models initialization

---
+++ src/main/java/crud/app/models/Member.java
@@ -0,0 +1,27 @@
+package crud.app.models;
+
+public class Member {
+    public String name;
+    public String BirthDay;
+
+    public Member(String name, String birthDay) {
+        this.name = name;
+        BirthDay = birthDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthDay() {
+        return BirthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        BirthDay = birthDay;
+    }
+}
