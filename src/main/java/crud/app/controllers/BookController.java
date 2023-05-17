14949229a98c83c4985e34280d1ed37d9043623c
thalassa_home <blinov.gosha@mail.ru>
2023-05-17 18:01:52 +0300

    controllers and dao initialize

---
+++ src/main/java/crud/app/controllers/BookController.java
@@ -0,0 +1,9 @@
+package crud.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+}
---
+++ src/main/java/crud/app/dao/MemberDAO.java
@@ -0,0 +1,7 @@
+package crud.app.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDAO {
+}
