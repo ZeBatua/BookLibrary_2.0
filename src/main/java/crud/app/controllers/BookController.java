cb0ad6dfc33c2bcbfb2f003c67702c65af5615d7
thalassa_home <blinov.gosha@mail.ru>
2023-05-18 21:12:03 +0300

    BookController + libraryPage.html

--- src/main/java/crud/app/controllers/BookController.java
+++ src/main/java/crud/app/controllers/BookController.java
@@ -1,9 +1,29 @@
 package crud.app.controllers;
 
+import crud.app.dao.BookDAO;
+import crud.app.dao.MemberDAO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/library")
 public class BookController {
+
+    private final BookDAO bookDAO;
+//    private final PersonValidator personValidator;
+
+    public BookController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("bookList", bookDAO.index());
+        return "library/libraryPage";
+    }
+
+
+
 }
