a5baf1e59f69726bf0f510914baf99c2d051e5db
thalassa_home <blinov.gosha@mail.ru>
2023-05-18 21:13:29 +0300

    add id in objects

--- src/main/java/crud/app/models/Book.java
+++ src/main/java/crud/app/models/Book.java
@@ -1,16 +1,26 @@
 package crud.app.models;
 
 public class Book {
+    private int id;
     private String name;
     private String author;
     private int year;
 
     public Book(){};
 
-    public Book(String name, String author, int year) {
+    public Book(int id, String name, String author, int year) {
+        this.id = id;
         this.name = name;
         this.author = author;
         this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -36,4 +46,5 @@ public class Book {
     public void setYear(int year) {
         this.year = year;
     }
+
 }
--- src/main/java/crud/app/models/Member.java
+++ src/main/java/crud/app/models/Member.java
@@ -1,14 +1,24 @@
 package crud.app.models;
 
 public class Member {
+    private int id;
     public String name;
     public String BirthDay;
 
     public Member(){}
 
-    public Member(String name, String birthDay) {
+    public Member(int id, String name, String birthDay) {
+        this.id = id;
         this.name = name;
         BirthDay = birthDay;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
