88d2bb085ad69d7a785f31d5d0a260812df299db
thalassa_home <blinov.gosha@mail.ru>
2023-06-13 19:44:35 +0300

    added BookService

--- src/main/java/crud/app/repositories/BookRepository.java
+++ src/main/java/crud/app/repositories/BookRepository.java
@@ -2,7 +2,9 @@ package crud.app.repositories;
 
 import crud.app.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 //    Optional<Member> findByOwner();
 }
--- src/main/java/crud/app/repositories/MemberRepository.java
+++ src/main/java/crud/app/repositories/MemberRepository.java
@@ -2,9 +2,11 @@ package crud.app.repositories;
 
 import crud.app.models.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByEmail(String email);
 }
